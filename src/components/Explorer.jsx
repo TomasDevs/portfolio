48b0020b395c589942b9a30523e7f5bb746b6fc9
Tomáš Štveráček <tomas.stveracek@gmail.com>
2024-10-03 22:14:30 +0200

    Add NavLink (isActive)

--- src/components/Explorer.jsx
+++ src/components/Explorer.jsx
@@ -1,5 +1,6 @@
 import "../styles/global.scss";
 import "../styles/Explorer.scss";
+import { NavLink } from "react-router-dom";
 
 const Explorer = () => {
   return (
@@ -9,29 +10,69 @@ const Explorer = () => {
         Portfolio
         <ul className="explorer">
           <li className="explorer__item">
-            <img src="/icons/icon-react.svg" width={20} alt="React icon" />
-            <span className="explorer__name">home.jsx</span>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "explorer__link -active" : "explorer__link"
+              }>
+              <img src="/icons/icon-react.svg" width={20} alt="React icon" />
+              <span className="explorer__name">home.jsx</span>
+            </NavLink>
           </li>
-          <li className="explorer__item">
-            <img src="/icons/icon-html.svg" width={20} alt="HTML icon" />
-            <span className="explorer__name">about.html</span>
-          </li>
-          <li className="explorer__item">
-            <img src="/icons/icon-css.svg" width={20} alt="CSS icon" />
-            <span className="explorer__name">contact.css</span>
-          </li>
-          <li className="explorer__item">
-            <img src="/icons/icon-js.svg" width={20} alt="JavaScript icon" />
-            <span className="explorer__name">projects.js</span>
-          </li>
-          <li className="explorer__item">
-            <img src="/icons/icon-json.svg" width={20} alt="JSON icon" />
 
-            <span className="explorer__name">articles.json</span>
-          </li>
           <li className="explorer__item">
-            <img src="/icons/icon-md.svg" width={20} alt="Markdown icon" />
-            <span className="explorer__name">github.md</span>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive ? "explorer__link -active" : "explorer__link"
+              }>
+              <img src="/icons/icon-html.svg" width={20} alt="HTML icon" />
+              <span className="explorer__name">about.html</span>
+            </NavLink>
+          </li>
+
+          <li className="explorer__item">
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                isActive ? "explorer__link -active" : "explorer__link"
+              }>
+              <img src="/icons/icon-css.svg" width={20} alt="CSS icon" />
+              <span className="explorer__name">contact.css</span>
+            </NavLink>
+          </li>
+
+          <li className="explorer__item">
+            <NavLink
+              to="/projects"
+              className={({ isActive }) =>
+                isActive ? "explorer__link -active" : "explorer__link"
+              }>
+              <img src="/icons/icon-js.svg" width={20} alt="JavaScript icon" />
+              <span className="explorer__name">projects.js</span>
+            </NavLink>
+          </li>
+
+          <li className="explorer__item">
+            <NavLink
+              to="/articles"
+              className={({ isActive }) =>
+                isActive ? "explorer__link -active" : "explorer__link"
+              }>
+              <img src="/icons/icon-json.svg" width={20} alt="JSON icon" />
+              <span className="explorer__name">articles.json</span>
+            </NavLink>
+          </li>
+
+          <li className="explorer__item">
+            <NavLink
+              to="/github"
+              className={({ isActive }) =>
+                isActive ? "explorer__link -active" : "explorer__link"
+              }>
+              <img src="/icons/icon-md.svg" width={20} alt="Markdown icon" />
+              <span className="explorer__name">github.md</span>
+            </NavLink>
           </li>
         </ul>
       </div>
--- src/components/Tabsbar.jsx
+++ src/components/Tabsbar.jsx
@@ -1,5 +1,6 @@
 import "../styles/global.scss";
 import "../styles/Tabsbar.scss";
+import { NavLink } from "react-router-dom";
 
 const Tabsbar = () => {
   return (
@@ -7,29 +8,69 @@ const Tabsbar = () => {
       <div className="tabsbar__tabs">
         <ul className="tabsbar">
           <li className="tabsbar__item">
-            <img src="/icons/icon-react.svg" width={20} alt="React icon" />
-            <span className="tabsbar__name">home.jsx</span>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "tabsbar__link -active" : "tabsbar__link"
+              }>
+              <img src="/icons/icon-react.svg" width={20} alt="React icon" />
+              <span className="tabsbar__name">home.jsx</span>
+            </NavLink>
           </li>
-          <li className="tabsbar__item">
-            <img src="/icons/icon-html.svg" width={20} alt="HTML icon" />
-            <span className="tabsbar__name">about.html</span>
-          </li>
-          <li className="tabsbar__item">
-            <img src="/icons/icon-css.svg" width={20} alt="CSS icon" />
-            <span className="tabsbar__name">contact.css</span>
-          </li>
-          <li className="tabsbar__item">
-            <img src="/icons/icon-js.svg" width={20} alt="JavaScript icon" />
-            <span className="tabsbar__name">projects.js</span>
-          </li>
-          <li className="tabsbar__item">
-            <img src="/icons/icon-json.svg" width={20} alt="JSON icon" />
 
-            <span className="tabsbar__name">articles.json</span>
-          </li>
           <li className="tabsbar__item">
-            <img src="/icons/icon-md.svg" width={20} alt="Markdown icon" />
-            <span className="tabsbar__name">github.md</span>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive ? "tabsbar__link -active" : "tabsbar__link"
+              }>
+              <img src="/icons/icon-html.svg" width={20} alt="HTML icon" />
+              <span className="tabsbar__name">about.html</span>
+            </NavLink>
+          </li>
+
+          <li className="tabsbar__item">
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                isActive ? "tabsbar__link -active" : "tabsbar__link"
+              }>
+              <img src="/icons/icon-css.svg" width={20} alt="CSS icon" />
+              <span className="tabsbar__name">contact.css</span>
+            </NavLink>
+          </li>
+
+          <li className="tabsbar__item">
+            <NavLink
+              to="/projects"
+              className={({ isActive }) =>
+                isActive ? "tabsbar__link -active" : "tabsbar__link"
+              }>
+              <img src="/icons/icon-js.svg" width={20} alt="JavaScript icon" />
+              <span className="tabsbar__name">projects.js</span>
+            </NavLink>
+          </li>
+
+          <li className="tabsbar__item">
+            <NavLink
+              to="/articles"
+              className={({ isActive }) =>
+                isActive ? "tabsbar__link -active" : "tabsbar__link"
+              }>
+              <img src="/icons/icon-json.svg" width={20} alt="JSON icon" />
+              <span className="tabsbar__name">articles.json</span>
+            </NavLink>
+          </li>
+
+          <li className="tabsbar__item">
+            <NavLink
+              to="/github"
+              className={({ isActive }) =>
+                isActive ? "tabsbar__link -active" : "tabsbar__link"
+              }>
+              <img src="/icons/icon-md.svg" width={20} alt="Markdown icon" />
+              <span className="tabsbar__name">github.md</span>
+            </NavLink>
           </li>
         </ul>
       </div>
