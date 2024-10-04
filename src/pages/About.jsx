173fe31047bd56a57558c4a7031d65f6e4d16ec2
Tomáš Štveráček <tomas.stveracek@gmail.com>
2024-10-04 15:19:15 +0200

    Add about pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,66 @@
+import "../styles/About.scss";
+
 const About = () => {
   return (
-    <section className="about">
-      <h1>About Me</h1>
-      <p>
-        I am a full-stack web developer with a passion for creating beautiful
-        and functional websites. I have experience with HTML, CSS, JavaScript,
-        React, Node.js, Express, and MongoDB. I am always looking to learn new
-        technologies and improve my skills.
-      </p>
-    </section>
+    <div className="about-page">
+      <pre>
+        <code>
+          <span className="line-number">1</span> &lt;
+          <span className="element">!DOCTYPE</span> html&gt; {"\n"}
+          <span className="line-number">2</span> &lt;
+          <span className="element">html</span> lang="en"&gt; {"\n"}
+          <span className="line-number">3</span> &nbsp;&nbsp;&lt;
+          <span className="element">head</span>&gt; {"\n"}
+          <span className="line-number">4</span> &nbsp;&nbsp;&nbsp;&nbsp;&lt;
+          <span className="element">title</span>&gt;About Tomas Stveracek&lt;/
+          <span className="element">title</span>&gt; {"\n"}
+          <span className="line-number">5</span> &nbsp;&nbsp;&nbsp;&nbsp;&lt;
+          <span className="element">meta</span> charSet="UTF-8" /&gt; {"\n"}
+          <span className="line-number">6</span> &nbsp;&nbsp;&nbsp;&nbsp;&lt;
+          <span className="element">meta</span> name="viewport"
+          content="width=device-width, initial-scale=1.0" /&gt; {"\n"}
+          <span className="line-number">7</span> &nbsp;&nbsp;&lt;/
+          <span className="element">head</span>&gt; {"\n"}
+          <span className="line-number">8</span> &nbsp;&nbsp;&lt;
+          <span className="element">body</span>&gt; {"\n"}
+          <span className="line-number">9</span> &nbsp;&nbsp;&nbsp;&nbsp;&lt;
+          <span className="element">header</span>&gt; {"\n"}
+          <span className="line-number">10</span>{" "}
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;
+          <span className="element">h1</span>&gt;About Me&lt;/
+          <span className="element">h1</span>&gt; {"\n"}
+          <span className="line-number">11</span> &nbsp;&nbsp;&nbsp;&nbsp;&lt;/
+          <span className="element">header</span>&gt; {"\n"}
+          <span className="line-number">12</span> &nbsp;&nbsp;&nbsp;&nbsp;&lt;
+          <span className="element">section</span>&gt; {"\n"}
+          <span className="line-number">13</span>{" "}
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;
+          <span className="element">p</span>&gt;Hi, I'm Tomas Stveracek.
+          Frontend Web Developer experienced in HTML, CSS, JavaScript and
+          React.&lt;/<span className="element">p</span>&gt; {"\n"}
+          <span className="line-number">14</span>{" "}
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;
+          <span className="element">p</span>&gt;In my free time, I enjoy
+          cycling, hiking, walking, bikepacking, coffee and traveling.&lt;/
+          <span className="element">p</span>&gt; {"\n"}
+          <span className="line-number">15</span> &nbsp;&nbsp;&nbsp;&nbsp;&lt;/
+          <span className="element">section</span>&gt; {"\n"}
+          <span className="line-number">16</span> &nbsp;&nbsp;&nbsp;&nbsp;&lt;
+          <span className="element">footer</span>&gt; {"\n"}
+          <span className="line-number">17</span>{" "}
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;
+          <span className="element">p</span>&gt;&copy; 2024 Tomas Stveracek. All
+          rights reserved.&lt;/<span className="element">p</span>&gt; {"\n"}
+          <span className="line-number">18</span> &nbsp;&nbsp;&nbsp;&nbsp;&lt;/
+          <span className="element">footer</span>&gt; {"\n"}
+          <span className="line-number">19</span> &nbsp;&nbsp;&lt;/
+          <span className="element">body</span>&gt; {"\n"}
+          <span className="line-number">20</span> &lt;/
+          <span className="element">html</span>&gt; {"\n"}
+        </code>
+      </pre>
+    </div>
   );
 };
+
 export default About;
