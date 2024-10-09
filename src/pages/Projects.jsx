99077d54a4fcdaf65d024d0acd407e8153f5ee65
Tomáš Štveráček <tomas.stveracek@gmail.com>
2024-10-09 23:03:11 +0200

    Add loading in projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,17 +4,29 @@ import "../styles/Projects.scss";
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("/public/api/projects.json") // Get the projects from the API
       .then((response) => response.json())
-      .then((data) => setProjects(data))
-      .catch((error) => console.error("Error fatching projects:", error));
+      .then((data) => {
+        setProjects(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fatching projects:", error);
+        setLoading(false);
+      });
   }, []);
 
   return (
     <div className="projects-page">
       <h2 className="projects-page__title">My Projects</h2>
+      {loading && (
+        <div className="loader">
+          <div className="spinner"></div>
+        </div>
+      )}
       <div className="projects-page__grid">
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
