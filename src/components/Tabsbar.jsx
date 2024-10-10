a036bdd91475fb469303bcf0bb9b63225c2f18de
Tomáš Štveráček <tomas.stveracek@gmail.com>
2024-10-10 22:59:30 +0200

    Refactor Tabsbar

--- src/components/Tabsbar.jsx
+++ src/components/Tabsbar.jsx
@@ -1,77 +1,38 @@
 import "../styles/global.scss";
 import "../styles/Tabsbar.scss";
-import { NavLink } from "react-router-dom";
+import TabsbarItem from "./TabsbarItem";
 
 const Tabsbar = () => {
   return (
     <div className="tabsbar">
       <div className="tabsbar__tabs">
         <ul className="tabsbar">
-          <li className="tabsbar__item">
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? "tabsbar__link -active" : "tabsbar__link"
-              }>
-              <img src="/icons/icon-react.svg" width={20} alt="React icon" />
-              <span className="tabsbar__name">home.jsx</span>
-            </NavLink>
-          </li>
-
-          <li className="tabsbar__item">
-            <NavLink
-              to="/about"
-              className={({ isActive }) =>
-                isActive ? "tabsbar__link -active" : "tabsbar__link"
-              }>
-              <img src="/icons/icon-html.svg" width={20} alt="HTML icon" />
-              <span className="tabsbar__name">about.html</span>
-            </NavLink>
-          </li>
-
-          <li className="tabsbar__item">
-            <NavLink
-              to="/contact"
-              className={({ isActive }) =>
-                isActive ? "tabsbar__link -active" : "tabsbar__link"
-              }>
-              <img src="/icons/icon-css.svg" width={20} alt="CSS icon" />
-              <span className="tabsbar__name">contact.css</span>
-            </NavLink>
-          </li>
-
-          <li className="tabsbar__item">
-            <NavLink
-              to="/projects"
-              className={({ isActive }) =>
-                isActive ? "tabsbar__link -active" : "tabsbar__link"
-              }>
-              <img src="/icons/icon-js.svg" width={20} alt="JavaScript icon" />
-              <span className="tabsbar__name">projects.js</span>
-            </NavLink>
-          </li>
-
-          <li className="tabsbar__item">
-            <NavLink
-              to="/articles"
-              className={({ isActive }) =>
-                isActive ? "tabsbar__link -active" : "tabsbar__link"
-              }>
-              <img src="/icons/icon-json.svg" width={20} alt="JSON icon" />
-              <span className="tabsbar__name">articles.json</span>
-            </NavLink>
-          </li>
-
-          <li className="tabsbar__item">
-            <NavLink
-              to="/github"
-              className={({ isActive }) =>
-                isActive ? "tabsbar__link -active" : "tabsbar__link"
-              }>
-              <img src="/icons/icon-md.svg" width={20} alt="Markdown icon" />
-              <span className="tabsbar__name">github.md</span>
-            </NavLink>
-          </li>
+          <TabsbarItem to="/" icon="/icons/icon-react.svg" name="home.jsx" />
+          <TabsbarItem
+            to="/about"
+            icon="/icons/icon-html.svg"
+            name="about.html"
+          />
+          <TabsbarItem
+            to="/contact"
+            icon="/icons/icon-css.svg"
+            name="contact.css"
+          />
+          <TabsbarItem
+            to="/projects"
+            icon="/icons/icon-js.svg"
+            name="projects.js"
+          />
+          <TabsbarItem
+            to="/articles"
+            icon="/icons/icon-json.svg"
+            name="articles.json"
+          />
+          <TabsbarItem
+            to="/github"
+            icon="/icons/icon-md.svg"
+            name="github.md"
+          />
         </ul>
       </div>
     </div>
---
+++ src/components/TabsbarItem.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom";
+
+const TabsbarItem = ({ to, icon, name }) => {
+  return (
+    <li className="tabsbar__item">
+      <NavLink
+        to={to}
+        className={({ isActive }) =>
+          isActive ? "tabsbar__link -active" : "tabsbar__link"
+        }>
+        <img src={icon} width={20} alt={`${name} icon`} />
+        <span className="tabsbar__name">{name}</span>
+      </NavLink>
+    </li>
+  );
+};
+
+export default TabsbarItem;
