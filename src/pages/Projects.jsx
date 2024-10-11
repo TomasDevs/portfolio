2077f76e3b57d789db5014aae6e0e636575c3cfc
Tomáš Štveráček <tomas.stveracek@gmail.com>
2024-10-11 10:15:14 +0200

    Refactor projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,31 +7,38 @@ const Projects = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("/public/api/projects.json") // Get the projects from the API
-      .then((response) => response.json())
-      .then((data) => {
+    const fetchProjects = async () => {
+      try {
+        const response = await fetch("/public/api/projects.json");
+        if (!response.ok) {
+          throw new Error("Failed to fetch projects");
+        }
+        const data = await response.json();
         setProjects(data);
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      } finally {
         setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fatching projects:", error);
-        setLoading(false);
-      });
+      }
+    };
+
+    fetchProjects();
   }, []);
 
   return (
     <div className="projects-page">
       <h2 className="projects-page__title">My Projects</h2>
-      {loading && (
+      {loading ? (
         <div className="loader">
           <div className="spinner"></div>
         </div>
+      ) : (
+        <div className="projects-page__grid">
+          {projects.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </div>
       )}
-      <div className="projects-page__grid">
-        {projects.map((project) => (
-          <ProjectCard key={project.id} project={project} />
-        ))}
-      </div>
     </div>
   );
 };
