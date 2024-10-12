64ed84655984da444445f4e4a8e1367a35dee1c9
Tomáš Štveráček <tomas.stveracek@gmail.com>
2024-10-12 22:40:13 +0200

    Add svg icons to skills

--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -1,8 +1,9 @@
 import "../styles/SkillsCard.scss";
 
-const SkillsCard = ({ title, description, link }) => {
+const SkillsCard = ({ title, description, link, svg }) => {
   return (
     <a href={link} className="card">
+      {svg && <div className="card__icon">{svg}</div>}
       <h3 className="card__title">{title}</h3>
       <p className="card__description">{description}</p>
     </a>
